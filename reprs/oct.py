3ba8bc694c03c98cdd77b43da26547c65fc54411
Hewei Gao <ghw1515489084@gmail.com>
2025-07-18 13:29:04 +0200

    doc: remove wrong comments. We are actually using bar_offset.

--- reprs/oct.py
+++ reprs/oct.py
@@ -345,8 +345,7 @@ class OctupleEncoding:
             df_indices = []
             if hasattr(self, "_df_indices"):
                 df_indices = [int(self._df_indices[i]) for i in indices]
-            
-            # (Hewei 2025-03-23) (But I still used it to keep the same as RNBert's design) We don't do the bar_index_offset augmentation for this task to preserve context/target bar identification
+          
             if self._apply_random_bar_index_offset:
                 bar_index_min = 0
                 bar_index_max = 0
